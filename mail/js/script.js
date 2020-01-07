57fae4faf5c400401e846348f9612e29a19d1b68
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-07 17:21:28 +0100

    mailDef

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,10 +1,27 @@
 //alert('ciao')
 
 // MAIL ACCESSO
-var mail = ['pippo@mail.it', 'pluto@mail.com', 'paperino@mail.it', 'paperino@mail.com', 'topolino@mail.it'];
-console.log(mail);
+var mails = ['pippo@mail.it', 'pluto@mail.com', 'paperino@mail.it', 'paperino@mail.com', 'topolino@mail.it'];
+console.log(mails);
 
 
-// MAIL UTENTE 
+// MAIL UTENTE
 var mailUtente = prompt('Inserisci la tua mail');
 console.log(mailUtente);
+console.log(ciccio);
+
+//CONFRONTO MAIL
+var mailCheck = false;
+for (var i=0; i < mails.length; i++) {
+  var mail = mails[i];
+  if (mailUtente == mail) {
+    mailCheck = true;
+  }
+}
+console.log(mailCheck);
+if (mailCheck == false) {
+  console.log('Accesso negato! registrarsi per accedere');
+}
+else {
+  console.log('Cliente registrato! Ciao bentornato');
+}
