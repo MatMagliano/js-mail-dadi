c251bfd9b9996c5fb23d28ec4655ac7894794444
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-07 16:06:19 +0100

    inserimentoMAilUtente

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,4 +1,10 @@
 //alert('ciao')
 
+// MAIL ACCESSO
 var mail = ['pippo@mail.it', 'pluto@mail.com', 'paperino@mail.it', 'paperino@mail.com', 'topolino@mail.it'];
 console.log(mail);
+
+
+// MAIL UTENTE 
+var mailUtente = prompt('Inserisci la tua mail');
+console.log(mailUtente);
